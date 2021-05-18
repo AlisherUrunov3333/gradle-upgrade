c960b83f598f053cf1bdc241f140556b11c3a758
alisher.urunov <alisher.urunov@accenture.com>
2021-05-18 15:10:35 +0300

    Init

---
+++ src/main/java/com/alisher/gradleupgrade/GradleUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.alisher.gradleupgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleUpgradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleUpgradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisher/gradleupgrade/GradleUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisher.gradleupgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleUpgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
